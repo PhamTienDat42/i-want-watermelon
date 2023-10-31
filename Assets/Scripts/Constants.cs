83e595c8fa30a9eeb14628fd0fefba1159471423
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-11-01 00:58:19 +0700

    add constants for control setting prefs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,9 @@
 public class Constants
 {
     public const string GameScene = "Scenes/Game";
+
+    //Key
+    public const string SoundKey = "sou";
+    public const string EffectKey = "eff";
+    public const string HapticKey = "hapt";
 }
