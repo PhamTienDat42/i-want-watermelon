a54116437943ca198cce35e1fc549e05ef5eb52d
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-11-01 00:56:28 +0700

    add objectpoolmanager for control dic of pool

---
+++ Assets/Scripts/ObjectPools/ObjectPoolManager.cs
@@ -0,0 +1,51 @@
+﻿using GamePlay;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectPools
+{
+    public class ObjectPoolManager : MonoBehaviour
+    {
+        private readonly Dictionary<string, Pools<Fruits>> objectPools = new();
+
+        [SerializeField] private List<Fruits> fruitPrefabs; // Nếu bạn có nhiều loại đối tượng, sử dụng mảng hoặc List để chứa các prefab
+
+        void Start()
+        {
+            InitializeObjectPools();
+        }
+
+        void InitializeObjectPools()
+        {
+            foreach (Fruits fruitPrefab in fruitPrefabs)
+            {
+                Pools<Fruits> objectPool = new Pools<Fruits>(fruitPrefab, 5, transform);
+                objectPools.Add(fruitPrefab.name, objectPool);
+            }
+        }
+
+        public Fruits GetObject(string objectTypeName)
+        {
+            if (objectPools.TryGetValue(objectTypeName, out Pools<Fruits> objectPool))
+            {
+                return objectPool.GetObject();
+            }
+
+            Debug.LogError($"Object pool for type {objectTypeName} not found!");
+            return null;
+        }
+
+        public void ReturnObject(Fruits obj)
+        {
+            string objectTypeName = obj.name;
+            if (objectPools.TryGetValue(objectTypeName, out Pools<Fruits> objectPool))
+            {
+                objectPool.ReturnObject(obj);
+            }
+            else
+            {
+                Debug.LogError($"Object pool for type {objectTypeName} not found!");
+            }
+        }
+    }
+}
