1100198cc9ab3e37dc02732c115abd499ea18542
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-11-01 01:13:53 +0700

    fix sstop on BG

--- Assets/Scripts/Game/Fruits.cs
+++ Assets/Scripts/Game/Fruits.cs
@@ -6,6 +6,23 @@ namespace GamePlay
     {
         [SerializeField] private int fruitPoint;
 
+        private Rigidbody2D rb;
+        private const string BGTag = "Background";
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(BGTag))
+            {
+                rb.velocity = Vector2.zero;
+                rb.gravityScale = 0f;
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             Fruits otherFruit = other.gameObject.GetComponent<Fruits>();
@@ -15,6 +32,18 @@ namespace GamePlay
                 if (CanCombine(otherFruit))
                 {
                     CombineObjects(otherFruit);
+                }
+            }
+
+            if (!other.isTrigger)
+            {
+                // D?ng l?i ho?c th?c hi?n các hành ??ng khác tùy thu?c vào yêu c?u c?a b?n
+                Debug.Log("Stopped on the background");
+                // ??t v?n t?c c?a Rigidbody v? 0 ?? ??i t??ng d?ng l?i
+                if (rb != null)
+                {
+                    rb.velocity = Vector2.zero;
+                    rb.gravityScale = 0f; // T?t tr?ng l?c n?u c?n
                 }
             }
         }
@@ -31,7 +60,6 @@ namespace GamePlay
 
             gameObject.SetActive(false);
             otherFruit.gameObject.SetActive(false);
-            // T?o ra ??i t??ng m?i v?i giá tr? m?i (Ch?a implement logic t?o m?i ??i t??ng)
         }
     }
 }
