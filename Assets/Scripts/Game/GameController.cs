6375d9711f6dd1761094c739623c6bc6e9693f44
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-11-01 00:58:54 +0700

    add game scripts for control Gameplay

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,17 @@
+using ObjectPools;
+using UnityEngine;
+
+namespace GamePlay
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private ObjectPoolManager objectPoolManager;
+        [SerializeField] private GameObject parentFruits;
+
+
+        private void Start()
+        {
+
+        }
+    }
+}
