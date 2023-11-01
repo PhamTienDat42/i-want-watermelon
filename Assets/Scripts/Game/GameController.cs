3df4e02f9aad0117173a23726513ce20848411de
PhamTienDat42 <117574501+PhamTienDat42@users.noreply.github.com>
2023-11-02 01:43:07 +0700

    add Drag in gamecontrolelr to getfruit from pools

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using ObjectPools;
 using UnityEngine;
@@ -6,17 +7,19 @@ namespace GamePlay
 {
     public class GameController : MonoBehaviour
     {
-        [SerializeField] private ObjectPoolManager objectPoolManager;
         [SerializeField] private GameObject parentFruits;
         [SerializeField] private List<Sprite> fruitSprites;
         [SerializeField] private FruitPools fruitPools;
         [SerializeField] private Camera mainCamera;
 
-        private bool isClickable = false;
+        private bool isClickable = true;
+        private int score = 0;
+        public int WaterMelonCount { get; set; }
+
 
         private void Start()
         {
-
+            isClickable = true;
         }
 
         private void Update()
@@ -27,11 +30,12 @@ namespace GamePlay
         private void DragFruits()
         {
 #if UNITY_EDITOR
-            if (Input.GetMouseButtonDown(0)/* && isClickable == true*/)
+            if (Input.GetMouseButtonDown(0) && isClickable == true)
             {
                 var mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
                 var pos = new Vector3(mousePos.x, 5f, 0f);
-                fruitPools.GetFruitFromPoolNew(pos, Quaternion.identity);
+                //fruitPools.GetFruitFromPoolNew(pos);
+                StartCoroutine(Drag(pos));
             }
 #elif UNITY_ANDROID
             if(Input.touchCount > 0 && isClickable == true)
@@ -42,12 +46,20 @@ namespace GamePlay
 #endif
         }
 
+        private IEnumerator Drag(Vector3 pos)
+        {
+            isClickable = false;
+            fruitPools.GetFruitFromPoolNew(pos);
+            yield return new WaitForSeconds(1.5f);
+            isClickable = true;
+        }
+
         public int RandomFruits()
         {
             return Random.Range(1, 10);
         }
 
         public List<Sprite> FruitSprites => fruitSprites;
-        public ObjectPoolManager ObjectPoolManagerFruits => objectPoolManager;
+        public int Score { get => score; set => score = value; }
     }
 }
