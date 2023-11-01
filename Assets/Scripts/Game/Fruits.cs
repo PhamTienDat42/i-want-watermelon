4e13e12a553bd5ec9c4a5f254c86821772eab48c
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-01 21:45:30 +0700

    fix logic when combine fruits

--- Assets/Scripts/Game/Fruits.cs
+++ Assets/Scripts/Game/Fruits.cs
@@ -1,3 +1,4 @@
+ï»¿using ObjectPools;
 using UnityEngine;
 
 namespace GamePlay
@@ -5,45 +6,32 @@ namespace GamePlay
     public class Fruits : MonoBehaviour
     {
         [SerializeField] private int fruitPoint;
-
+        private GameController controller;
+        private FruitPools fruitPools;
+        private SpriteRenderer spriteRenderer;
         private Rigidbody2D rb;
         private const string BGTag = "Background";
 
         void Start()
         {
+            var controllerObj = GameObject.FindGameObjectWithTag("GameController");
+            controller = controllerObj.GetComponent<GameController>();
+            var fruitPoolobj = GameObject.FindGameObjectWithTag("FruitPools");
+            fruitPools = fruitPoolobj.GetComponent<FruitPools>();
+
+            spriteRenderer = GetComponent<SpriteRenderer>();
             rb = GetComponent<Rigidbody2D>();
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.gameObject.CompareTag(BGTag))
-            {
-                rb.velocity = Vector2.zero;
-                rb.gravityScale = 0f;
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            Fruits otherFruit = other.gameObject.GetComponent<Fruits>();
+            Fruits otherFruit = collision.gameObject.GetComponent<Fruits>();
 
             if (otherFruit != null)
             {
                 if (CanCombine(otherFruit))
                 {
                     CombineObjects(otherFruit);
-                }
-            }
-
-            if (!other.isTrigger)
-            {
-                // D?ng l?i ho?c th?c hi?n các hành ??ng khác tùy thu?c vào yêu c?u c?a b?n
-                Debug.Log("Stopped on the background");
-                // ??t v?n t?c c?a Rigidbody v? 0 ?? ??i t??ng d?ng l?i
-                if (rb != null)
-                {
-                    rb.velocity = Vector2.zero;
-                    rb.gravityScale = 0f; // T?t tr?ng l?c n?u c?n
                 }
             }
         }
@@ -53,13 +41,31 @@ namespace GamePlay
             return fruitPoint == otherFruit.fruitPoint;
         }
 
+        void SpawnNewFruit(int newPoints, Vector3 pos)
+        {
+            Fruits newFruit = fruitPools.GetFruitFromPoolNew(pos, transform.rotation);
+            newFruit.fruitPoint = newPoints;
+            newFruit.GetComponent<SpriteRenderer>().sprite = controller.FruitSprites[(newPoints-1)%9];
+            newFruit.gameObject.SetActive(true);
+        }
+
         private void CombineObjects(Fruits otherFruit)
         {
-            int newPointValue = fruitPoint + otherFruit.fruitPoint;
-            Debug.Log($"Combined {fruitPoint} + {otherFruit.fruitPoint} = {newPointValue}");
+            Fruits higherFruit = (transform.position.y > otherFruit.transform.position.y) ? this : otherFruit;
 
+            if (!gameObject.activeSelf && !otherFruit.gameObject.activeSelf)
+            {
+                return;
+            }
+            var newPoitnts = fruitPoint + 1;
             gameObject.SetActive(false);
             otherFruit.gameObject.SetActive(false);
+            SpawnNewFruit(newPoitnts, higherFruit.transform.position);
+            if (rb != null)
+            {
+                Vector2 forceDirection = (transform.position - higherFruit.transform.position).normalized;
+                rb.AddForce(forceDirection*3f, ForceMode2D.Impulse);
+            }
         }
     }
 }
