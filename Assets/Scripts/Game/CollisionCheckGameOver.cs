9cdf7d9f37e407a2ff26319d01fe349ba345b1fe
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-02 15:50:40 +0700

    add CollisionCheckGameOver and Fruit tag for check game over

---
+++ Assets/Scripts/Game/CollisionCheckGameOver.cs
@@ -0,0 +1,49 @@
+﻿using ObjectPools;
+using UnityEngine;
+
+namespace GamePlay
+{
+    public class CollisionCheckGameOver : MonoBehaviour
+    {
+        [SerializeField] private GameController controller;
+        [SerializeField] private FruitPools fruitPools;
+        private bool isColliding = false;
+        private readonly float collisionDuration = 3f;
+
+        private const string Fruit = "Fruit";
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(Fruit))
+            {
+                isColliding = true;
+                StartCoroutine(CheckCollisionDuration());
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag(Fruit))
+            {
+                isColliding = false;
+            }
+        }
+
+        private System.Collections.IEnumerator CheckCollisionDuration()
+        {
+            float startTime = Time.time;
+
+            while (isColliding)
+            {
+                if (Time.time - startTime > collisionDuration)
+                {
+                    controller.NextFruit.gameObject.SetActive(false);
+                    StartCoroutine(fruitPools.ReturnActiveFruitsAndScoreWithDelay(0.25f));
+                    controller.IsClickable = false;
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+    }
+}
