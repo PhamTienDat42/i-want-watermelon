8df019d86c9e895382a67776ecce72f35b6533f9
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-02 11:51:45 +0700

    add create bounds and timer in GameCOntroller

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -7,18 +7,22 @@ namespace GamePlay
 {
     public class GameController : MonoBehaviour
     {
+        [SerializeField] private GameView gameView;
         [SerializeField] private GameObject parentFruits;
         [SerializeField] private List<Sprite> fruitSprites;
         [SerializeField] private FruitPools fruitPools;
         [SerializeField] private Camera mainCamera;
-        //[SerializeField] private SpriteRenderer nextFruitSprite;
+        [SerializeField] private GameObject boxBoundParent;
 
-        private Fruits nextFruit;
+        private Fruits.Fruit nextFruit;
         private bool isClickable = true;
         private int score = 0;
         public int WaterMelonCount { get; set; }
 
-        private readonly Vector3 startPos = new Vector3(0f, 4f,0f);
+        private float startTime;
+
+
+        private readonly Vector3 startPos = new(0f, 4f,0f);
 
         private void Awake()
         {
@@ -28,12 +32,18 @@ namespace GamePlay
         private void Start()
         {
             isClickable = true;
+            startTime = Time.time;
             nextFruit = fruitPools.GetFruitFromPoolNew(startPos);
-            //nextFruitSprite.sprite = nextFruit.GetComponent<SpriteRenderer>().sprite;
+            CreateScreenBoundsColliders(mainCamera, boxBoundParent);
         }
 
         private void Update()
         {
+            //Timer
+            float elapsedTime = Time.time - startTime;
+            UpdateTimerText(elapsedTime);
+
+            //Game
             DragFruits();
         }
 
@@ -44,7 +54,6 @@ namespace GamePlay
             {
                 var mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
                 var pos = new Vector3(mousePos.x, 4f, 0f);
-                //fruitPools.GetFruitFromPoolNew(pos);
                 StartCoroutine(Drag(pos));
             }
 #elif UNITY_ANDROID
@@ -68,7 +77,6 @@ namespace GamePlay
             isClickable = false;
             nextFruit.transform.localPosition = pos;
             nextFruit.GetComponent<Rigidbody2D>().gravityScale = 1f;
-            //nextFruitSprite.sprite = nextFruit.GetComponent<SpriteRenderer>().sprite;
             yield return new WaitForSeconds(1.5f);
             nextFruit = fruitPools.GetFruitFromPoolNew(startPos);
             isClickable = true;
@@ -77,6 +85,34 @@ namespace GamePlay
         public int RandomFruits()
         {
             return Random.Range(1, 10);
+        }
+
+        private void CreateScreenBoundsColliders(Camera mainCamera, GameObject parent)
+        {
+            float screenHeight = mainCamera.orthographicSize * 2f;
+            float screenWidth = screenHeight * mainCamera.aspect;
+            CreateSingleBoundCollider("Top", new Vector2(screenWidth, 0.01f), new Vector3(0f, screenHeight / 2f, 0f), parent);
+            CreateSingleBoundCollider("Left", new Vector2(0.01f, screenHeight), new Vector3(-screenWidth / 2f, 0f, 0f), parent);
+            CreateSingleBoundCollider("Right", new Vector2(0.01f, screenHeight), new Vector3(screenWidth / 2f, 0f, 0f), parent);
+        }
+
+        private void CreateSingleBoundCollider(string objName, Vector2 size, Vector3 localPos, GameObject parent)
+        {
+            GameObject newBound = new(objName);
+            var collider = newBound.AddComponent<BoxCollider2D>();
+            collider.size = size;
+            newBound.transform.localPosition = localPos;
+            newBound.transform.parent = parent.transform;
+        }
+
+        //Timer
+        private void UpdateTimerText(float elapsedTime)
+        {
+            int hours = Mathf.FloorToInt(elapsedTime / 3600f);
+            int minutes = Mathf.FloorToInt((elapsedTime % 3600f) / 60f);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+
+            gameView.SetTimer(hours, minutes, seconds);
         }
 
         public List<Sprite> FruitSprites => fruitSprites;
