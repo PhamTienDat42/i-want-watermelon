90291487609996af03d3ee532d126fd90ce1b8d4
DESKTOP-FLQ10M9\datpt <datpham0136@gmail.com>
2023-11-02 21:18:36 +0700

    add shake phone method

--- Assets/Scripts/ObjectPools/FruitPools.cs
+++ Assets/Scripts/ObjectPools/FruitPools.cs
@@ -12,12 +12,13 @@ namespace ObjectPools
         [SerializeField] private List<Fruit> fruitPrefabs;
         [SerializeField] private int poolSize = 10;
         [SerializeField] private GameObject fruitsParents;
+        [SerializeField] private GameController controller;
+        [SerializeField] private GameView gameView;
 
         private List<Fruit> fruitPools;
         private int lastDeactivatedFruitIndex = -1;
-
-        [SerializeField] private GameController controller;
-        [SerializeField] private GameView gameView;
+        private readonly float shakeForce = 5f;
+        private readonly float shakeThreshold = 3f;
 
         private void Awake()
         {
@@ -36,6 +37,25 @@ namespace ObjectPools
                 obj.gameObject.SetActive(false);
                 fruitPools.Add(obj);
             }
+        }
+
+        private void Update()
+        {
+            //ShakePhone
+            if (Input.acceleration.sqrMagnitude >= shakeThreshold * shakeThreshold && controller.BoolShake == true)
+            {
+                StartCoroutine(ShakePhone());
+            }
+        }
+
+        private IEnumerator ShakePhone()
+        {
+            controller.BoolShake = false;
+            controller.NextFruit.gameObject.SetActive(false);
+            ApplyShakeForce();
+            yield return new WaitForSeconds(2.0f);
+            controller.NextFruit.gameObject.SetActive(true);
+            controller.ResetCountDownTime(180f);
         }
 
         public Fruit GetFruitFromPoolNew(Vector3 position)
@@ -93,9 +113,39 @@ namespace ObjectPools
             fruit.gameObject.SetActive(false);
         }
 
+        public void ReturnFruitToPoolRandom(Fruit fruit)
+        {
+            var randomFruit = GetRandomFruitPrefab();
+            fruit.SpriteRenderer.sprite = randomFruit.GetComponent<SpriteRenderer>().sprite;
+            fruit.FruitPoint = randomFruit.FruitPoint;
+            fruit.CircleCollider2D.radius = randomFruit.GetComponent<CircleCollider2D>().radius;
+            fruit.gameObject.SetActive(false);
+        }
+
         private Fruit GetRandomFruitPrefab()
         {
             return fruitPrefabs[UnityEngine.Random.Range(0, 5)];
+        }
+
+        //Shake Phone
+        private void ApplyShakeForce()
+        {
+            Vector2 shakeDirection = Random.insideUnitCircle.normalized;
+            Vector2 shakeForce = shakeDirection * this.shakeForce;
+            var rigidList = new List<Rigidbody2D>();
+
+            foreach (var fruit in fruitPools)
+            {
+                if (fruit.gameObject.activeSelf)
+                {
+                    rigidList.Add(fruit.Rb);
+                }
+            }
+
+            foreach (var rb in rigidList)
+            {
+                rb.AddForce(shakeForce, ForceMode2D.Impulse);
+            }
         }
     }
 }
